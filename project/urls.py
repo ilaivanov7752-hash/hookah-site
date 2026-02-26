e85ffd9bcd7470cfb3e249a2e45e16804c5d4316
ilaivanov7752-hash <ilaivanov7752@gmail.com>
2026-02-26 22:09:15 +0300

    'robo'

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+
+    path('robots.txt', RedirectView.as_view(url='/static/robots.txt', permanent=True)),
+
 ]
