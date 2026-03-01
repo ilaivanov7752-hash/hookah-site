9f5ee3346c04f0a40323627cc9f6e72acb6a7589
ilaivanov7752-hash <ilaivanov7752@gmail.com>
2026-03-01 20:30:58 +0300

    'sav'

--- project/urls.py
+++ project/urls.py
@@ -16,12 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 
-    path('robots.txt', RedirectView.as_view(url='robots.txt', permanent=True)),
+    path('robots.txt', TemplateView.as_view(
+        template_name='robots.txt', 
+        content_type='text/plain'
+    )),
+    path('sitemap.xml', TemplateView.as_view(
+        template_name='sitemap.xml', 
+        content_type='application/xml'
+    )),
 
 ]
